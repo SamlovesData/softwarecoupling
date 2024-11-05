977da78f2a6627b5e9d8b9d6664ab42838210926
SamisData <148009124+SamlovesData@users.noreply.github.com>
2024-11-05 16:38:21 -0500

    Initial Release.

---
+++ src/main/java/edu/cpcc/labs/SoftwarecouplingApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwarecouplingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwarecouplingApplication.class, args);
+	}
+
+}
